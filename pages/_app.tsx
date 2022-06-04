68a1c4586b18d573786cb674d8e61f8a4cb91408
Gonzalo <gcuadros@gmail.com>
2022-06-04 13:19:02 +0200

    feat(WH-01): rewrite basepath and create header component with logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+
+import { ChakraProvider, Heading } from '@chakra-ui/react';
+
+import type { AppProps } from 'next/app';
+import Header from './components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Heading>
+        <Header />
+      </Heading>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/universities/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import type { NextPage } from 'next';
+
+const Universities: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Wuolah | Estudiante, gana dinero con tus apuntes.</title>
+        <meta name="title" content="Wuolah.com"></meta>
+        <meta
+          name="description"
+          content="Gana dinero compartiendo tus apuntes, y descarga los de tu clase de forma rápida y sencilla."
+        ></meta>
+        <meta name="keyword" content="Apuntes"></meta>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </>
+  );
+};
+
+export default Universities;
